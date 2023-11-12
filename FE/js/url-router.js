56ae748de891a90ac3d1050126b0e830b0327564
Mateo pizzolo <mateo@Mateos-Mac-mini.local>
2023-11-12 19:08:42 +0100

    asd

--- FE/js/url-router.js
+++ FE/js/url-router.js
@@ -45,20 +45,16 @@ const urlRoute = (event) => {
 
 // create a function that handles the url location
 const urlLocationHandler = async () => {
-	const location = window.location.pathname; // get the url path
-	// if the path length is 0, set it to primary page route
-	if (location.length == 0) {
-		location = "/";
-	}
-	// get the route object from the urlRoutes object
+	const location = window.location.pathname;
+	// if (location.length === 0) {
+	// 	// window.location.href = "/";
+	// 	return;
+	// }
+
 	const route = urlRoutes[location] || urlRoutes["404"];
-	// get the html from the template
 	const html = await fetch(route.template).then((response) => response.text());
-	// set the content of the content div to the html
 	document.getElementById("content").innerHTML = html;
-	// set the title of the document to the title of the route
 	document.title = route.title;
-	// set the description of the document to the description of the route
 	document
 		.querySelector('meta[name="description"]')
 		.setAttribute("content", route.description);
@@ -67,6 +63,11 @@ const urlLocationHandler = async () => {
 // add an event listener to the window that watches for url changes
 window.onpopstate = urlLocationHandler;
 // call the urlLocationHandler function to handle the initial url
-window.route = urlRoute;
-// call the urlLocationHandler function to handle the initial url
+// window.route = urlRoute;
 urlLocationHandler();
+
+// if (window.location.pathname === "/") {
+//     window.location.href = "/";
+// } else {
+//     urlLocationHandler();
+// }
